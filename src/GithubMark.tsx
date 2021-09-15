fec20ec3f31e00f91b8201dd0f1632c6bd8ffe52
Harri Virtanen <harri@niodi.fi>
2021-09-15 15:33:59 +0300

    Add light version of GithubMark

--- src/GithubMark.tsx
+++ src/GithubMark.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import image from './static/GitHub-Mark-64px.png'
+import image from './static/GitHub-Mark-Light-64px.png'
 
 const GithubMark = () => {
   return (
-    <a href="https://github.com/renodesu/sonar" target="_blank" rel="noreferrer" className="githubMark">
-      <img src={image} alt="" width={32} height={32} />
+    <a
+      href="https://github.com/renodesu/sonar"
+      target="_blank"
+      rel="noreferrer"
+      className="githubMark"
+      title="Check out the source code on Github! https://github.com/renodesu/sonar"
+    >
+      <img src={image} width={32} height={32} />
     </a>
   )
 }
