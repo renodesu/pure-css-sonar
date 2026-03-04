7041ec372ffe2f1f8269dc220c2c5235a863d4fb
Harri Virtanen <harri@niodi.fi>
2026-03-04 09:40:15 +0200

    feat: modernize

--- .stylelintrc.cjs
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: '@renodesu/stylelint-config',
-}
--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ function App() {
   return (
     <div className="App">
       <div className="sonar">
-        <div className="ring radius-20"></div>
-        <div className="ring radius-40"></div>
-        <div className="ring radius-60"></div>
-        <div className="ring radius-80"></div>
-        <div className="ping"></div>
-        <div className="sweep"></div>
-        <div className="gridLine"></div>
-        <div className="gridLine rotate-45"></div>
-        <div className="gridLine rotate-90"></div>
-        <div className="gridLine rotate-135"></div>
+        <div className="ring radius-20" />
+        <div className="ring radius-40" />
+        <div className="ring radius-60" />
+        <div className="ring radius-80" />
+        <div className="ping" />
+        <div className="sweep" />
+        <div className="gridLine" />
+        <div className="gridLine rotate-45" />
+        <div className="gridLine rotate-90" />
+        <div className="gridLine rotate-135" />
       </div>
       <GithubMark />
     </div>
